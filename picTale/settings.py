4441b01fc05a192a43d859804464e7e58bdf5030
vipul-pawar <vipulpawar311@gmail.com>
2025-05-31 14:10:36 +0530

    Allowed host render

--- picTale/settings.py
+++ picTale/settings.py
@@ -33,9 +33,9 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'pictale.onrender.com']
 
 
 # Application definition
