656bd8d8e783a3873761edc893805c29f7a72b5a
vipul-pawar <vipulpawar311@gmail.com>
2025-05-31 14:46:17 +0530

    updated setting.py

--- picTale/settings.py
+++ picTale/settings.py
@@ -33,7 +33,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pictale.onrender.com']
 
